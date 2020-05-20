d9677ab0513fa7a6c0e3a402054837be02b4d411
nandemotoken <nandemotoken@gmail.com>
2020-05-20 14:21:59 +0000

    typo

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+const faucetAddress = "0xB3d225839dAD0567FF713dDBdcEc7bCB6C2536B0"
+
+const ABI = [
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "faucetToken",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+document.onload = async () => {
+    const authereum = await new Authereum('mainnet')
+    const provider = await authereum.getProvider()
+    const web3 = await new Web3(provider) 
+    const faucet = await new web3.eth.Contract(ABI , faucetAddress)
+    const myaddress = await authereum.getAccountAddress()
+    ans = prompt("get reiwatoken?")
+    if (ans) {
+        faucet.methods.faucetToken().send({from: myaddress})
+    }
+}
